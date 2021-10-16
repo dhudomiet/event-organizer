e35f12aa877e15a4a2d4658efe3c394ffe05dfef
Damian <damian.hudomiet@gmail.com>
2021-10-16 22:55:56 +0200

    add Title and Footer components

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/compat/app"
+
+import 'firebase/compat/storate'
+import 'firebase/compat/firebase'
+
+const firebaseConfig = {
+
+apiKey: "AIzaSyB9NAdGbBZ2DPxNu3ntMK9ZnCUg3EjC0ho",
+authDomain: "event-organizer-802c1.firebaseapp.com",
+projectId: "event-organizer-802c1",
+storageBucket: "event-organizer-802c1.appspot.com",
+messagingSenderId: "1059399735915",
+appId: "1:1059399735915:web:a22df4767cb874d83cef2e"
+
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const firestore = firebase.firestore();
+
+export default {firestore};
---
+++ src/static/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <span className="footer-text">This is app created to learn React, HTML and CSS</span>
+        </div>
+    )
+}
+
+export default Footer;
